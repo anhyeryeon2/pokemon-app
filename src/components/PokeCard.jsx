fcf2d85a7e5c169c2debca1df9822b9c89fe9dd7
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-09-29 22:50:26 +0900

    검색기능 추가

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,63 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import LazyImage from './LazyImage';
-const PokeCard = ({url,name}) => {
-    const[pokemon, setPokemon] = useState();
+import { Link } from 'react-router-dom'; // react-router-dom에서 Link 가져오기
 
-    useEffect(()=>{
-        fetchPokeDetailData();
-    },[])
+const PokeCard = ({ url, name }) => {
+  const [pokemon, setPokemon] = useState();
 
-    async function fetchPokeDetailData(){
-        try{
-            const response = await axios.get(url)
-            const pokemonData = formatPokemonData(response.data);   
-            setPokemon(pokemonData); 
-        }catch(error){
-            console.error(error)
-        }
+  useEffect(() => {
+    fetchPokeDetailData();
+  }, []);
+
+  async function fetchPokeDetailData() {
+    try {
+      const response = await axios.get(url);
+      const pokemonData = formatPokemonData(response.data);
+      setPokemon(pokemonData);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    function formatPokemonData(params){
-        const{id,types,name} = params;
-        const PokeData = {
-            id,name,type:types[0].type.name
-        }
-        return PokeData;
-    }
+  function formatPokemonData(params) {
+    const { id, types, name } = params;
+    const PokeData = {
+      id,
+      name,
+      type: types[0].type.name,
+    };
+    return PokeData;
+  }
 
-    const bg = `bg-${pokemon?.type}`
-    const border = `border-${pokemon?.type}`
-    const text = `text-${pokemon?.type}`
-    const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`
-  
-    return (
-      <>
-        {pokemon && (
-          <a
-            href={`/pokemon/${name}`}
-            className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-50 bg-slate-800 justify-between items-center`}
+  const bg = `bg-${pokemon?.type}`;
+  const border = `border-${pokemon?.type}`;
+  const text = `text-${pokemon?.type}`;
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+
+  return (
+    <>
+      {pokemon && (
+        <a
+          href={`/pokemon/${name}`} 
+          className={`bg-slate-900 box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 justify-between items-center`}
+        >
+          <div
+            className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`}
           >
+            #{pokemon.id.toString().padStart(3, "00")}
+          </div>
+          <div className={`w-full f-6 flex items-center justify-center`}>
             <div
-              className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2  text-right rounded-t-lg`}
+              className={`box-border relative flex w-full h-[5.5rem] basis justify-center items-center`}
             >
-              #{pokemon.id.toString().padStart(3, "00")}
+              <LazyImage url={img} alt={name} />
             </div>
-            <div className={`w-full f-6 flex items-center justify-center`}>
-              <div
-                className={`box-border relative flex w-full h-[5.5rem] basis justify-center items-center`}
-              >
-            
-                <LazyImage
-                url={img}
-                  alt={name}
-                />
-              </div>
-            </div>
-          </a>
-        )}
-      </>
-    );
+          </div>
+          <div
+            className={`${bg} text-center text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1`}
+          >
+            {pokemon.name}
+          </div>
+        </a>
+      )}
+    </>
+  );
 };
 
 export default PokeCard;
